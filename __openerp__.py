a1b81af12ad31af11e99047b1671239c297a9f3e
Adel27 <r-27adel@live.fr>
2016-02-17 12:07:20 +0100

    Initial commit
    There is template report in views folder

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Serial reporting",
+    'summary': """Design Odoo for Soulco Client""",
+    'description': """
+        Reporting for Soulco project
+    """,
+    'author': "IGPro,
+    'website': "http://igpro-online.com",
+    'category': 'Product',
+    'version': '0.1',
+
+    'depends': ['sale','stock'],
+
+    'data': [
+        # 'views\serial_quotation_report.xml',
+        # 'views\serial_purchase_order_report.xml',
+    ],
+}
