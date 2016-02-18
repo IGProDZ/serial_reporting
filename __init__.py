f11592f993d206bdaaceb766949a6ed83b04258f
Sidwise <sid_wise@hotmail.com>
2016-02-18 09:17:51 +0100

    Adding purchase/sale reports

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import serial_report
+from . import soulcoreport
--- __openerp__.py
+++ __openerp__.py
@@ -1,19 +1,23 @@
+
 # -*- coding: utf-8 -*-
 {
-    'name': "Serial reporting",
-    'summary': """Design Odoo for Soulco Client""",
-    'description': """
-        Reporting for Soulco project
-    """,
-    'author': "IGPro,
+    'name': "reporting custum soulco",
+
+    'summary': """Design Odoo for Soulco Client Reports""",
+
+    'description': """ Reporting for Soulco project """,
+
+    'author': "IGPro",
     'website': "http://igpro-online.com",
     'category': 'Product',
     'version': '0.1',
 
-    'depends': ['sale','stock'],
+    'depends': ['sale','stock','purchase'],
 
     'data': [
-        # 'views\serial_quotation_report.xml',
-        # 'views\serial_purchase_order_report.xml',
+        'views/serial_quotation_report.xml',
+        'views/serial_purchase_order_report.xml',
+        'views/serial_reporting_picking_custum.xml',
     ],
+
 }
