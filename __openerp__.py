fed5394bbb8b56679cc9fb016bc6f5db19d7c5ee
Adel27 <r-27adel@live.fr>
2016-02-20 10:25:28 +0100

    Add Ref L, Ref S and Arrete on porchase order report

--- __openerp__.py
+++ __openerp__.py
@@ -15,9 +15,10 @@
     'depends': ['sale','stock','purchase'],
 
     'data': [
-        'views/serial_quotation_report.xml',
-        'views/serial_purchase_order_report.xml',
-        'views/serial_reporting_picking_custum.xml',
+        'views/soulco_purchase_report.xml',
+        # 'views/serial_quotation_report.xml',
+        # 'views/serial_purchase_order_report.xml',
+        # 'views/serial_reporting_picking_custum.xml',
     ],
 
 }
